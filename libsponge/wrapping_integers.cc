359b12eee1b39f2d058dc971286957bafcf4cd9c
ysw1912 <793377164@qq.com>
2022-08-13 21:04:16 +0000

    impl WrappingInt32

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,21 +1,15 @@
 #include "wrapping_integers.hh"
 
-// Dummy implementation of a 32-bit wrapping integer
+#include <cassert>
 
-// For Lab 2, please replace with a real implementation that passes the
-// automated checks run by `make check_lab2`.
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
-using namespace std;
+constexpr uint64_t k2ToPow32 = 1ull << 32;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t seq_no = (n % k2ToPow32 + static_cast<uint64_t>(isn.raw_value())) % k2ToPow32;
+    return WrappingInt32{static_cast<uint32_t>(seq_no)};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +23,26 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint32_t offset_32;
+    if (isn.raw_value() <= n.raw_value()) {
+        offset_32 = n - isn;
+    } else {
+        offset_32 = std::numeric_limits<uint32_t>::max() - isn.raw_value() + 1 + n.raw_value();
+    }
+    auto offset = static_cast<uint64_t>(offset_32);
+    if (checkpoint <= offset) {
+        return offset;
+    }
+    //                           checkpoint
+    //                               |
+    // offset | k2ToPow32 | ... | k2ToPow32 |
+    //                          |           |
+    //                        abs_seq_1         abs_seq_2
+    //        |<--=- count ---->|
+    //
+    uint64_t count = (checkpoint - offset) >> 32;
+    uint64_t abs_seq_1 = offset + k2ToPow32 * count;
+    uint64_t abs_seq_2 = offset + k2ToPow32 * (count + 1);
+    // Choose the seq no closest to the checkpoint.
+    return checkpoint - abs_seq_1 <= abs_seq_2 - checkpoint ? abs_seq_1 : abs_seq_2;
 }
